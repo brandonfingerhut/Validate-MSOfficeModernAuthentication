94c33c69d91ec5cd7a7ee8b62416e4b9ddc704c3
Brandon Fingerhut <brandonfingerhut@gmail.com>
2021-09-14 13:30:56 -0500

    Added test to ensure the Identity Key Path exists and if it does not to create it

--- Validate-ModernAuthentication.ps1
+++ Validate-ModernAuthentication.ps1
@@ -1,13 +1,13 @@
 <#
 .SYNOPSIS
-Enables Modern Authentication Registry Keys
+Enables Modern Authentication Registry Keys for Microsoft Office 2013 and 2016
 
 .DESCRIPTION
 Enables Modern Authentication Registry Keys as desribed in https://docs.microsoft.com/en-us/microsoft-365/admin/security-and-compliance/enable-modern-authentication?view=o365-worldwide
 
 Compatible with Office 2013 and 2016
 
-Version 1.0.0
+Version 1.0.1
 #>
 
 Clear-Host
@@ -15,18 +15,22 @@ Clear-Host
 Write-Host "Detecting version of Office installed"
 [int] $OfficeVersion =  (Invoke-Command -scriptblock{ (New-Object -comobject outlook.application).version}).substring(0,2)
 
-if ($OfficeVersion -eq 16)
-{
+if ($OfficeVersion -eq 16) {
     Write-Host "Version of office detected is Microsoft Office 2016"
-    $RegistryPath = 'HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Identity'
+    $OfficeIdentityKeyPath = 'HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Identity'
 }
-if ($OfficeVersion -eq 15)
-{
+if ($OfficeVersion -eq 15) {
     Write-Host "Version of office detected is Microsoft Office 2013"
-    $RegistryPath = 'HKCU:\SOFTWARE\Microsoft\Office\15.0\Common\Identity'
+    $OfficeIdentityKeyPath = 'HKCU:\SOFTWARE\Microsoft\Office\15.0\Common\Identity'
 }
 
-$OfficeConfig = Get-ItemProperty -Path $RegistryPath
+# If Identity Key does not exist
+if (-not(Test-Path $OfficeIdentityKeyPath)) {
+    $OfficeIdentityParentKeyPath = $OfficeIdentityKeyPath -replace "\Identity", ""
+    New-Item -Path $OfficeIdentityParentKeyPath -Name Identity
+}
+
+$OfficeConfig = Get-ItemProperty -Path $OfficeIdentityKeyPath
 
 if (($OfficeConfig.Version -eq 1) -and ($OfficeConfig.EnableADAL -eq 1)) {
     Write-Host "Microsoft Office is configured for Modern Authentication" -ForegroundColor Green    
@@ -34,13 +38,13 @@ if (($OfficeConfig.Version -eq 1) -and ($OfficeConfig.EnableADAL -eq 1)) {
     Write-Host "Microsoft Office is not configured for Modern Authentication" -ForegroundColor Red
     Write-Host "Enabling Modern Authentication"
     if (($OfficeConfig.Version -ne 1) -and ($OfficeConfig.Version -ne 0)) {
-        New-ItemProperty -Path $RegistryPath -Name Version -Value 1 -PropertyType DWORD -Force | Out-Null
+        New-ItemProperty -Path $OfficeIdentityKeyPath -Name Version -Value 1 -PropertyType DWORD -Force | Out-Null
     }
     if (($OfficeConfig.EnableADAL -ne 1) -and ($OfficeConfig.EnableADAL -ne 0)) {
-        New-ItemProperty -Path $RegistryPath -Name EnableADAL -Value 1 -PropertyType DWORD -Force | Out-Null
+        New-ItemProperty -Path $OfficeIdentityKeyPath -Name EnableADAL -Value 1 -PropertyType DWORD -Force | Out-Null
     }
     
-    $OfficeConfig = Get-ItemProperty -Path $RegistryPath
+    $OfficeConfig = Get-ItemProperty -Path $OfficeIdentityKeyPath
 
     if (($OfficeConfig.Version -eq 1) -and ($OfficeConfig.EnableADAL -eq 1)) {
         Write-Host "Microsoft Office is now configured for Modern Authentication" -ForegroundColor Green
